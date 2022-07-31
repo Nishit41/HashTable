fee97a352376f3402cd20e3e0be33d0d9d8422ae
Nishit <nishitranjan41@gmail.com>
2022-07-31 23:28:47 +0530

    this is my hashyable

---
+++ src/com/bridgelabz/hashtable/LinkedList.java
@@ -0,0 +1,56 @@
+package com.bridgelabz.hashtable;
+public class LinkedList<K,V> {
+        MyMapNode<K,V> head;
+        MyMapNode<K,V> tail;
+
+
+        public MyMapNode<K,V> search(K searchData) {
+            MyMapNode<K,V> temp = head;
+            while(temp != null){
+                if(temp.key.equals(searchData))
+                    return temp;
+                temp = temp.next;
+            }
+            return null;
+        }
+
+
+        public void delete(K key){
+            MyMapNode<K,V> temp = head;
+            MyMapNode<K,V> searchedElement = search(key);
+            while(temp.next != searchedElement){
+                temp = temp.next;
+            }
+            temp.next = searchedElement.next;
+        }
+
+
+        public void append(MyMapNode<K,V> node ) {
+
+            if(head == null) {
+                head = node;
+                tail = node;
+            }
+            else{
+                tail.next = node;
+                tail = node;
+            }
+        }
+
+        public void print(){
+            MyMapNode<K,V> temp = head;
+            while(temp != null){
+                System.out.println(temp);
+                temp = temp.next;
+            }
+        }
+
+        @Override
+        public String toString() {
+            return "LinkedList{" +
+                    "head=" + head +
+                    ", tail=" + tail +
+                    '}';
+        }
+
+    }
